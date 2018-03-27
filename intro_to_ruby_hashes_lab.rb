cf9a6a1875031888757593547b283fe6678ec28e
Megan Dotto <megan.dotto@gmail.com>
2018-03-27 22:12:28 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ end
 
 def monopoly_with_third_tier
 	monopoly = {
-		:railroads => {pieces: 4, names: {}, rent_in_dollars: 25}
+		:railroads => {pieces: 4, names: {}, rent_in_dollars: "25"}
 	}
 end
 
